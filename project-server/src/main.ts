e35cc61e43972fc1b21a6039cb1166818be72043
Camila-AF23 <camilaalarcon900@gmail.com>
2026-05-12 15:29:36 -0500

    instalacion del JWT

--- project-server/src/main.ts
+++ project-server/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from '../config/enviroment';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(PORT ?? 3000);
 }
 void bootstrap();
